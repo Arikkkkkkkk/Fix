9a4eda1ecbfd4f5de3c479f399a756ce76871cb5
nameless <havanvuong.dmx@gmail.com>
2026-08-01 12:23:33 +0700

    maybe itll fix the issue idfk

--- src/util/shulkerglobals.h
+++ src/util/shulkerglobals.h
@@ -3,3 +3,9 @@
 inline int  g_shulkerCacheIndex = -1;
 inline char g_shulkerColorCode  = '0';
 inline bool g_hasShulkerData    = false;
+
+inline bool  g_shulkerHasLastCursor = false;
+inline float g_shulkerLastCursorX   = 0.0f;
+inline float g_shulkerLastCursorY   = 0.0f;
+
+constexpr float kShulkerDragThresholdSq = 8.0f;
